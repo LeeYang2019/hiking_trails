cacece784782f86eba741dbb4be2773827870cdf
leeyang2019 <nyang@madisoncollege.edu>
2022-06-15 16:25:02 -0500

    fixed routes so api is showing

--- controller/trail.js
+++ controller/trail.js
@@ -1,11 +1,14 @@
-// @desc    POST an activity
-// @route   POST /api/v1/activities
+// @desc    POST a trail
+// @route   POST /api/v1/trails
 // @access  Private
 exports.addTrail = async (req, res) => {
 	console.log(req);
 };
 
-exports.getTrail = (req, res) => {
+// @desc	GET all trails
+// @route	GET /api/v1/trails
+// @access	Public
+exports.getTrails = (req, res) => {
 	console.log('hello');
 
 	res.status(200).json({ message: `entered trails path` });
--- routes/trail.js
+++ routes/trail.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { addTrail, getTrail } = require('../controller/trail');
+const { addTrail, getTrails } = require('../controller/trail');
 
 const router = express.Router();
 
-router.get('/').get(getTrail);
-router.post('/:id/addPost').post(addTrail);
-
+router.route('/').get(getTrails);
+router.route('/addPost').post(addTrail);
+//need to get for user
+//need to patch
+//need to delete
 module.exports = router;
